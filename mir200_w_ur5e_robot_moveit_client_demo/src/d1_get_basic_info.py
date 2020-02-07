161d2221edb2fa6460e8bda1af7aeac11b8ae161
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2020-02-07 16:16:26 +0900

    Add moveit_client for demo

---
+++ mir200_w_ur5e_robot_moveit_client_demo/src/d1_get_basic_info.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+if __name__ == "__main__":
+    # Initialize moveit_commander and node
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('d1_get_basic_info', anonymous=False)
+
+    # Get instance from moveit_commander
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+
+    # Get group_commander from MoveGroupCommander
+    group_name = "arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+
+    # Get information
+    planning_frame = move_group.get_planning_frame()
+    eef_link = move_group.get_end_effector_link()
+    group_names = robot.get_group_names()
+    current_state = robot.get_current_state()
+
+    print "============ Planning frame: %s" % planning_frame
+    print "============ End effector link: %s" % eef_link
+    print "============ Available Planning Groups:", robot.get_group_names()
+    print "============ Printing robot state"
+    print current_state
+    print "="*20
+
+    quit()
+
---
+++ mir200_w_ur5e_robot_moveit_client_demo/src/d5-4_remove_box.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+from tf.transformations import *
+
+
+if __name__ == "__main__":
+    # Initialize moveit_commander and node
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('d1_get_basic_info', anonymous=False)
+
+    # Get instance from moveit_commander
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+
+    # Get group_commander from MoveGroupCommander
+    group_name = "arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    rospy.sleep(1.0)
+
+
+    # Remove the box from world
+    scene.remove_world_object("box")
+
+    quit()
